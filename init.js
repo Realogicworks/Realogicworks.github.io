811bbceea073b632b658a056ca3590e9b7db37bd
ZakharZakharov <zakhar.v.zakharov@gmail.com>
2022-11-15 19:51:33 +0200

    First commit

---
+++ init.js
@@ -0,0 +1,29 @@
+function checkAppObject(){
+	if(window.MyApp && window.userpilot && window.MyApp.loginUserData)
+		return true;
+	return false;
+}
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function identifyUser(){
+	while(!checkAppObject()){
+		await timeout(500);
+	}
+	userpilot.identify(MyApp.loginUserData.Email,{email: MyApp.loginUserData.Email});
+}
+
+(async() => {
+  await identifyUser();    
+})();
+
+
+window.addEventListener('hashchange', (event) => {    
+    if(checkAppObject()){
+    userpilot.track("Page navigation", {
+        url: event.newURL.replace(window.location.origin,'')        
+      });            
+    }
+  }, false);
